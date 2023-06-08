dc346cc338875a931d9bad3ff44b2c9b1b6b575e
Samshuleeecuet <samshuleeeweb@gmail.com>
2023-06-09 00:19:08 +0600

    MongoConnection Issue Fix

--- index.js
+++ index.js
@@ -9,8 +9,8 @@ app.use(express.json())
 
 const { MongoClient, ServerApiVersion } = require('mongodb');
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.df1ioxo.mongodb.net/?retryWrites=true&w=majority`;
+console.log(process.env.DB_USER,process.env.DB_PASS)
 
-// Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
   serverApi: {
     version: ServerApiVersion.v1,
@@ -19,21 +19,20 @@ const client = new MongoClient(uri, {
   }
 });
 async function run() {
-    try {
-      // Connect the client to the server	(optional starting in v4.7)
-      await client.connect();
-      // Send a ping to confirm a successful connection
-      await client.db("admin").command({ ping: 1 });
-      console.log("Pinged your deployment. You successfully connected to MongoDB!");
-    }
- finally {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+
+
+
+  } finally {
     // Ensures that the client will close when you finish/error
     //await client.close();
   }
 }
-
-
-
 app.get('/',(req,res)=>{
     res.send('Language Center Server Running')
 })
