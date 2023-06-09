d912cb1855ef0d46e6e8a9486acf48cdad5d214b
Samshuleeecuet <samshuleeeweb@gmail.com>
2023-06-09 11:55:11 +0600

    create class route

--- index.js
+++ index.js
@@ -9,8 +9,6 @@ app.use(express.json())
 
 const { MongoClient, ServerApiVersion } = require('mongodb');
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.df1ioxo.mongodb.net/?retryWrites=true&w=majority`;
-console.log(process.env.DB_USER,process.env.DB_PASS)
-
 const client = new MongoClient(uri, {
   serverApi: {
     version: ServerApiVersion.v1,
@@ -20,19 +18,34 @@ const client = new MongoClient(uri, {
 });
 async function run() {
   try {
-    // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
-    // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
 
+    // Database connection
+    const database = client.db("LanguageCenter");
+    const classes = database.collection("classes");
+    const users = database.collection("users");
+
+    // CLasses Route 
+
+    app.get('/classes',async(req,res)=>{
+      const result = await classes.find().toArray();
+      res.send(result)
+    })
+    app.post('/classes',async(req,res)=>{
+      const myclass = req.body;
+      const result = await classes.insertOne(myclass)
+      res.send(result)
+    })
 
 
   } finally {
-    // Ensures that the client will close when you finish/error
     //await client.close();
   }
 }
+
+run()
 app.get('/',(req,res)=>{
     res.send('Language Center Server Running')
 })
